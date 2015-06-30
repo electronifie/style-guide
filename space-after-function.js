71f93b91222b922adef1f3f3179932c57b23a0e3
Andrew McKenzie <andrew.mckenzie@electronifie.com>
2015-06-30 16:54:05 -0400

    Fix globs.

--- space-after-function.js
+++ space-after-function.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'space-after-function',
-  appliesTo: '*.(js|coffee)',
+  appliesTo: '*.+(js|coffee)',
   validate: function (lines) {
     return lines.map(function (line) { return line.replace(/function\(/, 'function ('); });
   }
