69edac2da1c92044a0a5a54612dab0b0bbd26f3c
Andrew McKenzie <andrew.mckenzie@electronifie.com>
2015-07-01 11:01:50 -0400

    Add script that displays files being checked.

---
+++ parrot.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'parrot',
+  decription: 'Lets the user know we\'re checking its commits. Doesn\'t make suggestions.',
+  validate: function (lines, options) { console.log('☕ Checking %s line%s in %s.', lines.length, lines.length == 1 ? '' : 's', options.fileName); return lines; }
+};
