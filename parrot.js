deb2a89cf4c8095b0b4e1baaaa11bee77dc3d38d
Andrew McKenzie <andrew.mckenzie@electronifie.com>
2015-07-10 15:48:54 -0400

    Make parrot more robust.

--- parrot.js
+++ parrot.js
@@ -1,5 +1,8 @@
 module.exports = {
   name: 'parrot',
   decription: 'Lets the user know we\'re checking its commits. Doesn\'t make suggestions.',
-  validate: function (lines, options) { console.log('☕ Checking %s line%s in %s.', lines.length, lines.length == 1 ? '' : 's', options.fileName); return lines; }
+  validate: function (lines, options) {
+    console.log('☕ Checking %s line%s in %s.', lines.length, lines.length == 1 ? '' : 's', (options && options.fileName) || '??');
+    return lines;
+  }
 };
